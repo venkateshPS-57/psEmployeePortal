06e55763e0d760380c984dbe5491db104772fd9d
venkateshPS-57 <154536162+venkateshPS-57@users.noreply.github.com>
2024-01-09 12:05:44 +0530

    Add files via upload

---
+++ logout.spec.ts
@@ -0,0 +1,27 @@
+import { test, expect } from "@playwright/test";
+
+// Importing all required pages from POM
+import loginPage from "../pages/loginPage";
+import BasePage from "../pages/basepage";
+import DashboardPage from "../pages/dashboardPage";
+
+//importing testdata's
+const testData = JSON.parse(JSON.stringify(require("../testData.json")));
+
+// Test --URL
+const testURL = testData.baseUrl.ProductionUrl;
+
+// Test --User Credential
+const testUserMail = testData.employee.mail;
+const testUserPassword = testData.employee.password;
+
+test("LogoutScript", async ({ page }) => {
+  const login = new loginPage(page);
+  const dashboard = new DashboardPage(page);
+  const base = new BasePage(page);
+
+  await base.waitTillNetworkIdle();
+  await login.goToLoginPage(testURL);
+  await login.loginWithEmailAndPassword(testUserMail, testUserPassword);
+  await dashboard.clickOnLogout();
+});
